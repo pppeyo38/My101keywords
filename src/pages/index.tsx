37ba06e725fc6fff1817084ac7fc4272369a3ab5
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-12-10 17:28:16 +0900

    ➕ Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export default function Home() {
   return (
@@ -13,5 +13,5 @@ export default function Home() {
         <p>テスト</p>
       </main>
     </div>
-  );
+  )
 }
