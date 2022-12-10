c0d417ec9668c8b76b447357e41e02babce23ff9
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-12-10 18:08:35 +0900

    🎨 add global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
+import { Global, css } from '@emotion/react'
 import type { AppProps } from 'next/app'
+import emotionReset from 'emotion-reset'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionReset}
+          body {
+            font-family: 'Zen Kaku Gothic New', sans-serif;
+            letter-spacing: 0.03em;
+            background-color: #fff;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="My 101 KeyWords" />
+          <meta property="og:description" content="My 101 KeyWords" />
+          <meta property="og:site_name" content="My 101 KeyWords" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
